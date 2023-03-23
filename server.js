91e8cf71df77ce14b9da6a6637238d15d04bc651
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2023-03-23 17:37:02 -0600

    Comentarios

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express')
+const express = require('express') //Inyeccion de la dependencia
 const app = express()
-let port = process.env.PORT || 3000;
-app.use('/assets', express.static(__dirname + '/public'));
+let port = process.env.PORT || 3000; //Definicion del puerto de escucha
+app.use('/assets', express.static(__dirname + '/public')); //Contenido estatico
 
 app.use(express.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs');
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) { //render de vista student
   res.render('student')
 })
 
@@ -22,9 +22,9 @@ app.get('/', function (req, res) {
                 Tipo de sangre ${req.body.tipoSangre}`)
 })*/
 
-app.post('/addStudent', function(req, res){
-    res.render('displayData', {
-        nombre: req.body.nombre,
+app.post('/addStudent', function(req, res){ //render de los datos guardados del formulario
+    res.render('displayData', { 
+        nombre: req.body.nombre, //Datos que plasmaremos en nuestra vista
         edad: req.body.edad,
         nss: req.body.nss,
         tipoSangre: req.body.tipoSangre
